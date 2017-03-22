b99c7510bd16157956609ca23d22062f409c6319
Conner <Conner@10.80.149.27>
2017-03-22 10:53:40 -0400

    Inital Check

---
+++ CS1120CMTHLA5/src/edu/wmich/cs1120/la5/ExpressionFactory.java
@@ -0,0 +1,14 @@
+package edu.wmich.cs1120.la5;
+
+public class ExpressionFactory {
+	
+//	public static IExpression getExpression(char operator, int val1, int val2) {
+//		
+//		IExpression ex = new IExpression();
+//		
+//		
+//		return new ex;
+//	}
+	
+
+}
---
+++ CS1120CMTHLA5/src/edu/wmich/cs1120/la5/IRover.java
@@ -0,0 +1,15 @@
+package edu.wmich.cs1120.la5;
+
+import java.util.*;
+
+public interface IRover {
+	
+	ArrayList<IArea> getPath();
+	void setPath(ArrayList<IArea> path);
+	void analyzePath();
+	String getAnalysis();
+	void setAnalysis(String analysis);
+	String toString();
+	
+	
+}
